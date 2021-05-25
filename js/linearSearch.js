2a5f89183df8843d3049ca55cca638d6383138d9
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-25 14:55:08 -0500

    Shawn and Stephen Linear Search Solution

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,21 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
+        if (arrayToSearchThrough[i] === valueToFind) {
+            return i;
+        }
+    }
 
+    return undefined;
 };
 
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+    let output = [];
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
+        if (arrayToSearchThrough[i] === valueToFind) {
+            output.push(i);
+        }
+    }
+    return output;
+}
--- js/linearSearchSpec.js
+++ js/linearSearchSpec.js
@@ -8,3 +8,6 @@ console.log(ls.linearSearch(13, [1,2,3]) === undefined);
 console.log(shallowEqualArrays(ls.linearSearchGlobal("a", ["b", "a", "n", "a", "n", "a", "s"]), [1, 3, 5]))
 console.log(shallowEqualArrays(ls.linearSearchGlobal("s", ["b", "a", "n", "a", "n", "a", "s"]), [6]))
 console.log(shallowEqualArrays(ls.linearSearchGlobal("n", ["b", "a", "n", "a", "n", "a", "s"]), [2, 4]))
+
+console.log(shallowEqualArrays(ls.linearSearchGlobal("x", ["b", "a", "n", "a", "n", "a", "s"]), []))
+console.log(shallowEqualArrays(ls.linearSearchGlobal(2, ["b", "a", "n", "a", "n", "a", "s", 2]), [7]))
