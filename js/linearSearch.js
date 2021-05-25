a55fc192adfbd051d0b71451b6e136f5967daf03
SarahShinespark <sarahshinespark@users.noreply.github.com>
2021-05-25 22:17:41 +0000

    Sarah and Kyle LSG finished

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,7 +1,4 @@
-import { shallowEqualArrays } from "shallow-equal";
-//console.log(ls.linearSearch(3, [1,2,3]) === 2);
-//console.log(ls.linearSearch(4, [1,2,3]) === undefined);
-//console.log(ls.linearSearch(13, [1,2,3]) === undefined);
+//import { shallowEqualArrays } from "shallow-equal";
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
   
@@ -15,15 +12,17 @@ exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
 //console.log(shallowEqualArrays(ls.linearSearchGlobal("a", ["b", "a", "n", "a", "n", "a", "s"]), [1, 3, 5]))
 //console.log(shallowEqualArrays(ls.linearSearchGlobal("s", ["b", "a", "n", "a", "n", "a", "s"]), [6]))
 //console.log(shallowEqualArrays(ls.linearSearchGlobal("n", ["b", "a", "n", "a", "n", "a", "s"]), [2, 4]))
+var shallowEqualArrays = require('shallow-equal/arrays');
 
 exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
   let answer = [];
   for(let i=0; i<arrayToSearchThrough.length; i++){
       if(shallowEqualArrays(arrayToSearchThrough[i], valueToFind)){
-          answer.push(arrayToSearchThrough[i])
+          answer.push(i)
           //shallowEqualArrays([1, 2, 3], [1, 2, 3]);
       }
   }  
+  return answer
 };
 
 
