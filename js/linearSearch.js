eeca667c4f85e9a3570639aebf26abe8850bd69d
jung341416 <jung341416@gmail.com>
2021-05-25 17:04:47 -0400

    JS and JP linear0

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,15 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+    let targetVal = valueToFind;
+    let result = undefined;
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
 
-};
+        if(arrayToSearchThrough[i] == targetVal) {
+            result = i;
+        }
+    }
+    return result;
+}
+
 
