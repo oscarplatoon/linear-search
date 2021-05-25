3f9c8b261bd1fec3a2874cf502797be807a3bc02
SarahShinespark <sarahshinespark@users.noreply.github.com>
2021-05-25 21:49:40 +0000

    Sarah and Kyle pt1

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,9 +1,18 @@
-const arrayToSearchThrough = [];
-for (let i = 1; i <= 1000; i++) {
-    arrayToSearchThrough.push(i);
-}
+//const arrayToSearchThrough = [];
+//for (let i = 1; i <= 1000; i++) {
+//    arrayToSearchThrough.push(i);
+//}
+
+//console.log(ls.linearSearch(3, [1,2,3]) === 2);
+//console.log(ls.linearSearch(4, [1,2,3]) === undefined);
+//console.log(ls.linearSearch(13, [1,2,3]) === undefined);
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+  let answer = []
+  for(let i=0; i<arrayToSearchThrough.length; i++){
+      if(arrayToSearchThrough[i] === valueToFind){
+          return i
+      }
+  }  
 };
 
