e96dcef254a3803650d896b632104cf62574a4a4
SarahShinespark <sarahshinespark@users.noreply.github.com>
2021-05-25 22:00:25 +0000

    Sarah and Kyle LSG first attempt

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,18 +1,30 @@
-//const arrayToSearchThrough = [];
-//for (let i = 1; i <= 1000; i++) {
-//    arrayToSearchThrough.push(i);
-//}
-
+import { shallowEqualArrays } from "shallow-equal";
 //console.log(ls.linearSearch(3, [1,2,3]) === 2);
 //console.log(ls.linearSearch(4, [1,2,3]) === undefined);
 //console.log(ls.linearSearch(13, [1,2,3]) === undefined);
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-  let answer = []
+  
   for(let i=0; i<arrayToSearchThrough.length; i++){
       if(arrayToSearchThrough[i] === valueToFind){
           return i
       }
   }  
 };
+
+//console.log(shallowEqualArrays(ls.linearSearchGlobal("a", ["b", "a", "n", "a", "n", "a", "s"]), [1, 3, 5]))
+//console.log(shallowEqualArrays(ls.linearSearchGlobal("s", ["b", "a", "n", "a", "n", "a", "s"]), [6]))
+//console.log(shallowEqualArrays(ls.linearSearchGlobal("n", ["b", "a", "n", "a", "n", "a", "s"]), [2, 4]))
+
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+  let answer = [];
+  for(let i=0; i<arrayToSearchThrough.length; i++){
+      if(shallowEqualArrays(arrayToSearchThrough[i], valueToFind)){
+          answer.push(arrayToSearchThrough[i])
+          //shallowEqualArrays([1, 2, 3], [1, 2, 3]);
+      }
+  }  
+};
+
+
 
