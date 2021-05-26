71dcab6912b538b8072f3b3d54a6fea070467082
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-26 14:15:16 -0500

    Shawn's python solution

--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,19 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    
+    for index in range(len(array_to_search_through)):
+        if array_to_search_through[index] == value_to_find:
+            return index
+    
+    return None
+
+def global_linear_search(value_to_find, array_to_search_through):
+
+    values = []
+
+    for index in range(len(array_to_search_through)):
+        if array_to_search_through[index] == value_to_find:
+            values.append(index)
+    
+    return values
--- python/linear_search_spec.py
+++ python/linear_search_spec.py
@@ -1,5 +1,13 @@
 from linear_search import linear_search
+from linear_search import global_linear_search
 
 print(linear_search(3, [1,2,3]) == 2)
 print(linear_search(4, [1,2,3]) == None)
 print(linear_search(13, [1,2,3]) == None)
+
+print((global_linear_search("a", ["b", "a", "n", "a", "n", "a", "s"]) == [1, 3, 5]))
+print(global_linear_search("s", ["b", "a", "n", "a", "n", "a", "s"]) == [6])
+print(global_linear_search("n", ["b", "a", "n", "a", "n", "a", "s"]) == [2, 4])
+
+print(global_linear_search("x", ["b", "a", "n", "a", "n", "a", "s"]) == [])
+print(global_linear_search(2, ["b", "a", "n", "a", "n", "a", "s", 2]) == [7])
