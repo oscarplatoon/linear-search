7ea0472d9465ad9d8115491db7983e3e9551c4ce
Smattonline <smattonline@gmail.com>
2021-05-26 00:12:06 -0700

    Musa's Linear Search

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -3,7 +3,29 @@ for (let i = 1; i <= 1000; i++) {
     arrayToSearchThrough.push(i);
 }
 
-exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+function linearSearch(valueToFind, arrayToSearchThrough) {
+    
+    var indexLocations = []
 
+    
+    
+    for (var i = 0; i < arrayToSearchThrough.length; i++) {
+        if (typeof arrayToSearchThrough.indexOf(valueToFind) != "number") {
+            console.log("undefined")
+            break;
+        }
+      if (arrayToSearchThrough[i] == valueToFind) {
+          //indexLocations = []ac
+          indexLocations.push(i)
+      } 
+      
+    }
+    
+    return indexLocations
 };
 
+console.log(linearSearch(2, [1,2,3]))// === 1);
+console.log(linearSearch(13, [1,2,3]))// === undefined);
+
+console.log(linearSearch("a", ["b", "a", "n", "a", "n", "a", "s"]))//, [1, 3, 5]))
+console.log(linearSearch("s", ["b", "a", "n", "a", "n", "a", "s"]))//, [6]))
