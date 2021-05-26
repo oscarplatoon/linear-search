7fa4bdc32322df14c7d1fce792a3030611c6ccc1
Marc Stanley <marcson909@gmail.com>
2021-05-26 17:57:41 -0500

    completed py challenge using same logic as js

--- python/linear_search.py
+++ python/linear_search.py
@@ -1,6 +1,17 @@
-array_to_search_through = []
-for number in range(1, 1001):
-    array_to_search_through.append(number)
-
-def linear_search(value_to_find, array_to_search_through):
+def linear_search(value_to_find,array_to_search_through):
     # your code here
+    for x in range(len(array_to_search_through)):
+        if array_to_search_through[x] == value_to_find:
+            return x
+            
+    else:
+        return None
+
+def global_linear_search(value_to_find,array_to_search_through):
+    result_arr = []
+
+    for x in range(len(array_to_search_through)):
+        if array_to_search_through[x] == value_to_find:
+            result_arr.append(x)
+    return result_arr
+    
