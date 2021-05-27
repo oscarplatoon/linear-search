d6178e0fd5030807fc0bbc138ca397395da6f187
Ankur Shah <ankur@codeplatoon.org>
2021-05-27 09:12:04 -0500

    completed release 1 in PY

--- python/linear_search.py
+++ python/linear_search.py
@@ -13,4 +13,16 @@ def linear_search(value_to_find, array_to_search_through):
 
     # if we don't find our value, return None
     return None
+
+
+def global_linear_search(value_to_find, array_to_search_through):
+    found_index_list = []
+    i = 0
+    while i < len(array_to_search_through):
+        if array_to_search_through[i] == value_to_find:
+            # if we find our value, store the index
+            found_index_list.append(i)
+        i += 1
+
+    return found_index_list
     
