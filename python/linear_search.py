7d8c34a10f292a75738371c2b0fb5f0d76114f00
Ankur Shah <ankur@codeplatoon.org>
2021-05-27 09:04:40 -0500

    completed release 0 in PY

--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,14 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    # loop through array
+    i = 0
+    while i < len(array_to_search_through):
+        if array_to_search_through[i] == value_to_find:
+            # if we find our value, return the index
+            return i
+        i += 1
+
+    # if we don't find our value, return None
+    return None
+    
--- python/linear_search_spec.py
+++ python/linear_search_spec.py
@@ -3,3 +3,8 @@ from linear_search import linear_search
 print(linear_search(3, [1,2,3]) == 2)
 print(linear_search(4, [1,2,3]) == None)
 print(linear_search(13, [1,2,3]) == None)
+print(linear_search("5", [1,2,3,5]) == None)
+print(linear_search(1, [2,3,5,1,7,6]) == 3)
+print(linear_search(3, [2,3,5,1,7,6,3]) == 1)
+print(linear_search(99, [2,3,5,1,7,6,99]) == 6)
+print(linear_search(99.1, [2,3,5,1,7,6,99]) == None)
