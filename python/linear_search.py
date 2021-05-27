871473fea5a0a5e62cf65bad7b6b081e28520e16
vzbrunstad <vzbrunstad@msn.com>
2021-05-27 10:03:13 -0500

    Vince B Linear Search JS in progress PY complete

--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,19 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    i=0 
+    while i< len (array_to_search_through):
+        if array_to_search_through[i] == value_to_find:
+            return i
+        i += 1
+    return None
+
+def global_linear_search(value_to_find, array_to_search_through):
+    output = []
+    i=0 
+    while i< len (array_to_search_through):
+        if array_to_search_through[i] == value_to_find: # Enumerate
+            #If we find our value return index
+            output.append(i)
+        i += 1
+    return output
